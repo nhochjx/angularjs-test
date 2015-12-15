1b2bad31984685ada1b51813f2a26805d77184d6
kjmtrue <kimtrien@gmail.com>
2015-12-15 15:42:02 +0700

    Add route test in app

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res, next){
+    res.json({
+        demo: 'Test',
+    });
+});
+
 var port = app.get('port') || 3000;
 app.listen(port, function(){
     console.log('App listening on port: ', port);
