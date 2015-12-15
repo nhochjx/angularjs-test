3e2d8355344bbcac377b95beffe897dc0ccb1d46
kjmtrue <kimtrien@gmail.com>
2015-12-15 15:39:33 +0700

    Require route in app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+require('./route.js')(app);
+
 var port = app.get('port') || 3000;
 app.listen(port, function(){
     console.log('App listening on port: ', port);
