fac82a38e8a7e26c41ebb34d2c29c95ca29975e0
kjmtrue <kimtrien@gmail.com>
2015-12-15 15:53:36 +0700

    Update route hehe to app

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 require('./route.js')(app);
 
+app.get('/hehe', function(req, res, mext){
+    res.end('Test hehe');
+});
+
 var port = app.get('port') || 3000;
 app.listen(port, function(){
     console.log('App listening on port: ', port);
