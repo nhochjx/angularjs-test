2a3faca1f5da8be42157725c6e0fd3e9c7efe88b
kjmtrue <kimtrien@gmail.com>
2015-12-15 16:17:01 +0700

    Test add tag

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/xinchao', function(req, res, mext){
     res.end('Hello word');
 });
 
-var port = app.get('port') || 8000;
+var port = app.get('port') || 3000;
 app.listen(port, function(){
     console.log('App listening on port: ', port);
 })
